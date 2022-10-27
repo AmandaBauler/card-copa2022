b6aff0fd8ce87c2ade640e2b095ac693685eb1d5
AmandaBauler <104072024+AmandaBauler@users.noreply.github.com>
2022-10-27 10:47:11 -0300

    .

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let isIgnite = false
 function changeCard(event) {
   const card = event.currentTarget
-  const bg = isIgnite ? "explorer" : "ignite"
+  const bg = isIgnite 
+  ? "url(./assets/bg-explores.svg)" 
+  : "url(./assets/bg-ignite.svg)"
   isIgnite = !isIgnite
-  card.style.backgroundImage = `url(./assets/bg-${bg}.svg)`
+  card.style.backgroundImage = bg
 }
